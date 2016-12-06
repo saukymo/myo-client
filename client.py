27a947236b1603020349b97068e15f6452739634
saukymo <saukymo@gmail.com>
2016-12-06 23:32:00 +0800

    Stop waiting after connection.

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import myo
 import sys
-from socketIO_client import SocketIO, LoggingNamespace
-
+from socketIO_client import SocketIO
 
 device_id = 1
 server_url = "http://127.0.0.1/"
@@ -16,6 +15,28 @@ def unregister():
 
 def send_emg(emg, moving):
     socketIO.emit("emg", {"device_id":device_id, "emg":emg})
+
+class SocketIOClient(SocketIO):
+    """
+    Fix for library bug
+    """
+
+    def _should_stop_waiting(self, for_connect=False, for_callbacks=False):
+        if for_connect:
+            for namespace in self._namespace_by_path.values():
+                is_namespace_connected = getattr(
+                    namespace, '_connected', False)
+                #Added the check and namespace.path
+                #because for the root namespaces, which is an empty string
+                #the attribute _connected is never set
+                #so this was hanging when trying to connect to namespaces
+                # this skips the check for root namespace, which is implicitly connected
+                if not is_namespace_connected and namespace.path:
+                    return False
+            return True
+        if for_callbacks and not self._has_ack_callback:
+            return True
+        return super(SocketIO, self)._should_stop_waiting()
 
 if __name__ == "__main__":
     # myo_client = myo.MyoRaw(sys.argv[1] if len(sys.argv) >= 2 else None)
@@ -35,10 +56,12 @@ if __name__ == "__main__":
     #     unregister()
     #     print()
 
-    socketIO = SocketIO(server_url, 5000, LoggingNamespace)
+    socketIO = SocketIOClient(server_url, 5000)
+    register()
+
     m = MyoRaw(sys.argv[1] if len(sys.argv) >= 2 else None)
     m.add_emg_handler(send_emg)
-    
+
     def proc_emg(emg, moving, times=[]):
         print(emg)
 
